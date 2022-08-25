d8298c6c8a0582a87bbe3274ac2905011ee3ba92
ekatevundt <ekatevundt@yandex.ru>
2022-08-25 10:37:40 +0300

    Get longer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int [] sales = {3, 44, 3, 4, 5, 6};
+        long [] sales = {3, 44, 3, 4, 5, 6};
         SalesManager salesManager = new SalesManager(sales);
         salesManager.max();
         System.out.println(salesManager.max());
